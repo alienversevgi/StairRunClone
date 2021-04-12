2191bd447757779134d41b7ce0f9d916a324af24
Ali Enver <tiyago1@gmail.com>
2021-04-12 21:08:54 +0300

    Timer added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Timer : Singleton<Timer>
+{
+    public Coroutine StartTimer(float time, Action action)
+    {
+        return StartCoroutine(WaitAndExecute(time, action));
+    }
+
+    public void Stop(Coroutine timer)
+    {
+        if (timer != null)
+            StopCoroutine(timer);
+    }
+
+    public void StopAllCoroutine()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator WaitAndExecute(float time, Action action)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        action?.Invoke();
+    }
+}
