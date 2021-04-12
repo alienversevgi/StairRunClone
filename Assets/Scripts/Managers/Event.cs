c945210455b591c07d06f704461ebe1bd11ddaef
Ali Enver <tiyago1@gmail.com>
2021-04-12 21:07:05 +0300

    EventManager Added

---
+++ Assets/Scripts/Managers/Event.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Tiyago1.EventManager
+{
+    [CreateAssetMenu()]
+    public class Event : ScriptableObject
+    {
+        public event Action EventListeners;
+
+        public void Raise()
+        {
+            EventListeners.Invoke();
+        }
+
+        public void Register(Action listener)
+        {
+            EventListeners += listener;
+        }
+
+        public void UnregisterListener(Action listener)
+        {
+            EventListeners -= listener;
+        }
+    }
+}
--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -1,9 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventManager : MonoBehaviour
+namespace Tiyago1.EventManager
 {
+    public class EventManager : MonoBehaviour
+    {
+        #region Singelaton
 
-  
+        private static EventManager instance;
+
+        public static EventManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = GameObject.FindObjectOfType<EventManager>();
+                }
+
+                return instance;
+            }
+        }
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        #endregion
+
+        public Event OnCollectBrick;
+        public Event OnReduceBrick;
+    }
 }
